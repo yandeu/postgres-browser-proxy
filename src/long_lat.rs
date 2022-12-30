23055a2a8c0a99972cad079bb662fa00dda9b2b1
yandeu <20306025+yandeu@users.noreply.github.com>
2022-12-31 00:19:31 +0100

    dev

--- src/long_lat.rs
+++ src/long_lat.rs
@@ -11,6 +11,7 @@ pub struct LongLat {
     pub lat: f64,
     pub srid: Option<i32>,
 }
+
 impl LongLat {
     pub fn new(long: f64, lat: f64, srid: Option<i32>) -> Self {
         Self { long, lat, srid }
@@ -22,6 +23,7 @@ impl LongLat {
         )
     }
 }
+
 impl<'a> FromSql<'a> for LongLat {
     fn from_sql(_ty: &Type, raw: &'a [u8]) -> Result<Self, Box<dyn Error + Sync + Send>> {
         let mut buff = Cursor::new(raw);
@@ -36,23 +38,31 @@ impl<'a> FromSql<'a> for LongLat {
         buff.read_exact(&mut _u32).unwrap();
 
         let byte_order = i8::from_le_bytes(_i8);
-        let _is_be = byte_order == 0i8;
+        let is_be = byte_order == 0i8;
 
-        let type_id = u32::from_le_bytes(_u32);
+        let type_id = bytes_to_u32(&is_be, _u32);
         let mut srid: Option<i32> = None;
 
         if type_id & 0x20000000 == 0x20000000 {
             buff.read_exact(&mut _i32).unwrap();
-            srid = Some(i32::from_le_bytes(_i32));
+            srid = Some(bytes_to_i32(&is_be, _i32));
         }
 
         buff.read_exact(&mut _long).unwrap();
         buff.read_exact(&mut _lat).unwrap();
 
-        let long = f64::from_le_bytes(_long);
-        let lat = f64::from_le_bytes(_lat);
+        let long = bytes_to_f64(&is_be, _long);
+        let lat = bytes_to_f64(&is_be, _lat);
 
-        // see: https://postgis.net/docs/manual-dev/using_postgis_dbmanagement.html#OpenGISWKBWKT
+        // println!("type_id {}", type_id);
+        // println!("srid {}", srid.unwrap());
+        // println!("long {}", long);
+        // println!("lat {}", lat);
+
+        // see:
+        // - https://mariadb.com/kb/en/well-known-binary-wkb-format/
+        // - https://postgis.net/docs/manual-dev/using_postgis_dbmanagement.html#OpenGISWKBWKT
+        // - https://libgeos.org/specifications/wkb/
         let geo = match type_id & 0xff {
             0x01 => LongLat::new(long, lat, srid),
             _ => todo!(),
@@ -66,5 +76,29 @@ impl<'a> FromSql<'a> for LongLat {
             "geography" => true,
             _ => false,
         }
+    }
+}
+
+fn bytes_to_u32(is_be: &bool, bytes: [u8; 4]) -> u32 {
+    if *is_be {
+        u32::from_be_bytes(bytes)
+    } else {
+        u32::from_le_bytes(bytes)
+    }
+}
+
+fn bytes_to_i32(is_be: &bool, bytes: [u8; 4]) -> i32 {
+    if *is_be {
+        i32::from_be_bytes(bytes)
+    } else {
+        i32::from_le_bytes(bytes)
+    }
+}
+
+fn bytes_to_f64(is_be: &bool, bytes: [u8; 8]) -> f64 {
+    if *is_be {
+        f64::from_be_bytes(bytes)
+    } else {
+        f64::from_le_bytes(bytes)
     }
 }
